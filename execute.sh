be98c3d89ff0d5250c55f83dd7386f42520d5e8c
zouqing <qing.zou@timanetworks.com>
2020-04-16 18:11:54 +0800

    fixed compile

--- execute.sh
+++ execute.sh
@@ -6,5 +6,14 @@ autoheader
 autoconf
 automake --add-missing
 
+
+ ######################## x86 ########################
+ export FINAL_PATH=/home/zouqing/work/carnet/linux/auto/premake/x86/install
+ export FINAL_PATH=$PWD/install
+ export GOLBAL_CFLAGS="-I/usr/include -I$FINAL_PATH/include"
+ export GOLBAL_CPPFLAGS="-I/usr/include -I$FINAL_PATH/include"
+ export GOLBAL_LDFLAGS="-L/usr/lib -L$FINAL_PATH/lib"
+ export platform=ubuntu
+
 # ../configure --prefix=$PWD/../install --enable-examples
 
